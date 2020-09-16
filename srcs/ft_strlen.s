dcc19f0f6d909a77ac028bab5d6fea5ee434d4c2
Eder Junior DE SOUZA <esouza@student.42.fr>
2020-09-16 15:08:42 +0200

    Update ft_strlen
    Added a link to the official home page of the linux system call

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,3 +1,6 @@
+; torvalds / linux 64-bit system call numbers :
+; https://githut.com/torvalds/linux/blob/master/arch/x86/entry/syscalls/syscall_64.tbl
+
 ; The .text section is for the code
 section     .text
 ; global tell the kernel where the program executino begins
