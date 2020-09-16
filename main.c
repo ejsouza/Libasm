0082e28da34b342ccdc6ed293f5b0d769af5fad4
Eder Junior DE SOUZA <esouza@student.42.fr>
2020-09-16 23:08:33 +0200

    Create tests
    Created a main to start testing the lib.

---
+++ main.c
@@ -0,0 +1,19 @@
+# include "include/libasm.h"
+
+size_t      ft_strlen(const char *s);
+
+void strlen_test()
+{
+    printf("----- ft_strlen -----\n");
+    hello_world();
+    printf("lib : %zu\n", ft_strlen("Toto"));
+}
+
+int main(void)
+{
+    printf("Testing... %d\n", TOTO);
+
+
+    strlen_test();
+    return (0);
+}
