62f8d56a7ecd3aae85311b4fe5e74577981ee6ca
Eder Junior DE SOUZA <esouza@student.42.fr>
2020-09-19 09:08:05 +0200

    Update strcpy
    Changed how to set the value of the resgister to 0, instead of doing
    mov rcx, 0 now I'm using xor rcx, rcx as it's faster and cleaner

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -3,13 +3,13 @@ section .text
     global      ft_strcpy
 ;char *strcpy(char *dest, const char *src);    
 ft_strcpy:
-    mov         rcx, 0 ; rcx 64-bit counter register
+    xor         rcx, rcx ; rcx 64-bit counter register
 ; Parameter in order:     rdi, rsi, rdx, rcx, r8, r9
 ; Return values in order: rax, rdx
 ; strcpy(rdi, rsi) return (rax);
 loop:
-    mov     dh, BYTE[rsi + rcx] ; dh or dl 8-bit data register (enough here to hold one char)
-    mov     BYTE[rdi + rcx], dh
+    mov     dl, BYTE[rsi + rcx] ; dh(high H byte) or dl(low L byte) 8-bit data register (enough here to hold one char)
+    mov     BYTE[rdi + rcx], dl
     cmp     BYTE[rsi + rcx], 0
     je      return
     inc     rcx
