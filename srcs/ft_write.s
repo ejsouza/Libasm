656b67af196752ea8e716dc23502c8665f71fa59
Eder Junior DE SOUZA <esouza@student.42.fr>
2020-09-19 16:36:24 +0200

    Create ft_write function
    Created the write function, this function could be done in two lines of code
    as the parameter passed to it is the same we will pass to syscall, but for
    returning equal values as the one from the libc I added some extra checks
    to return a -1 when negatives or NULL values are passed.
    
    The function passed all the tests.

---
+++ srcs/ft_write.s
@@ -0,0 +1,25 @@
+section     .text
+
+    global   ft_write
+; ssize_t write(int fd, const void *buf, size_t count);
+; ssize_t write(rdi, rsi, rdx);
+ft_write:
+    mov         rax, 1 ; 1 is the value for system call in linux
+ 
+    ; jmp         check_negative ; could simply ret here but for negative it returns -14
+                               ; while the c version from the main return -1
+    
+check_negative:
+    test        rdx, rdx
+    js          negative
+    bt          rdi, 31  ;  check if last bit is set, what means is negative
+    jc          negative ; jc is a jump if confidtion is met  
+    cmp         rsi, 0x0 ; check for NULL pointer
+    je          negative
+    syscall
+    
+exit:
+    ret
+negative:
+    mov         rax, -1
+    ret
